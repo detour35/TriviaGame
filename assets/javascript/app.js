173a1b65e4ea4d20f832e9cf878b1e5cee06a0e0
detour35 <danelis19@gmail.com>
2017-12-13 20:28:36 -0500

    adding different questions to the game

---
+++ assets/javascript/app.js
@@ -0,0 +1,43 @@
+var question1 = {
+    question: 'What planet is farthest from the sun',
+    answers: ['jupiter', 'pluto', 'mars', 'earth'],
+    correct: 1
+  }
+  var question2 = {
+    question: 'What planet is farthest from the sun',
+    answers: ['jupiter', 'pluto', 'mars', 'earth'],
+    correct: 1
+  }
+  var question3 = {
+    question: 'What planet is farthest from the sun',
+    answers: ['jupiter', 'pluto', 'mars', 'earth'],
+    correct: 1
+  }
+  var question4 = {
+    question: 'What planet is farthest from the sun',
+    answers: ['jupiter', 'pluto', 'mars', 'earth'],
+    correct: 1
+  }
+  
+  
+  $('#question').html(question1.question);
+  
+  // METHOD 1
+//   answers to first question
+
+  $('#answer0').html(question1.answers[0]);
+  $('#answer1').html(question1.answers[1]);
+  $('#answer2').html(question1.answers[2]);
+  $('#answer3').html(question1.answers[3]);
+
+for (var i = 0; i < question)
+  
+// METHOD 2
+
+/*
+var answerDivs = $('.answer');
+
+for (var i = 0; i < answerDivs.length; i++) {
+  $(answerDivs[i]).html(question1.answers[i])
+}
+*/
